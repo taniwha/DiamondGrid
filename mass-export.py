80ec85e6255d770e69f0b8cd58807fae7d39330e
Bill Currie <bill@taniwha.org>
2017-05-05 10:59:22 +0900

    Export textures when exporting models.
    Also, remove mass-export.py from the blend file for easier maintenance.

--- mass-export.py
+++ mass-export.py
@@ -1,20 +1,27 @@
 import bpy
 import os
-from io_object_mu.export_mu import export_object
+from io_object_mu.export_mu import export_object, strip_nnn
+
+textures = set()
 
 blend_filepath = bpy.context.blend_data.filepath
 blend_filepath = os.path.dirname(blend_filepath)
 print(blend_filepath)
 for obj in bpy.data.objects:
     if not obj.parent and obj.children:
-        name = obj.name+".mu"
+        name = strip_nnn(obj.name)+".mu"
         filepath = os.path.join(blend_filepath, name)
+        print(name, filepath)
+
         mu = export_object (obj, filepath)
-        #for tex in mu.textures:
-        #    image = bpy.data.images[tex.name]
-        #    print(tex.name, image.type)
-        #    name = tex.name + ".png"
-        #    image.packed_files[0].filepath = os.path.join(blend_filepath, name)
-        #    #image.file_format = 'PNG'
-        #    #print(image.filepath_raw)
-        #    image.save()
+        for tex in mu.textures:
+            textures.add(tex.name)
+
+for tex in textures:
+    image = bpy.data.images[tex]
+    name = tex + ".png"
+    path = os.path.join(blend_filepath, name)
+    print(tex, image.type, path)
+    image.filepath_raw = "//" + name
+    image.packed_files[0].filepath = path
+    image.save()
